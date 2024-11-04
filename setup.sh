7ed86d68026667cd696962f27cef56a88d845468
Just Playin <ensminger@yahoo.com>
2024-11-04 11:54:37 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+sudo apt update
+sudo apt install xfce4 xfce4-goodies curl -y
+sudo apt install xrdp -y
+cd ~
+echo "xfce4-session" | tee .xsession
+sudo ufw allow from $(curl ifconfig.me)/32 to any port 3389
+sudo systemctl restart xrdp
+clear
+sudo ufw status
+sudo systemctl status xrdp
+exit
